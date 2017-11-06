256a36603c81a3737edf51dcfaa3fcfa41adfe80
Larisa Kreismanis <lkreismanis@luc.edu>
2017-11-06 14:49:27 +0000

    First Commit

--- src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
+++ src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
@@ -3,6 +3,8 @@ package edu.luc.cs271.wordcount;
 import java.util.Comparator;
 import java.util.Map;
 
+//Larisa Kreismanis
+
 /** The comparison strategy for sorting the array by the word count. */
 public class DescendingByCount implements Comparator<Map.Entry<String, Integer>> {
   public int compare(final Map.Entry<String, Integer> l, final Map.Entry<String, Integer> r) {
